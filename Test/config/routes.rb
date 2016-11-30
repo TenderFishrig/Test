2a402c9a3cfe18c19aee9b7addef4fed1d124b7e
Liam <tender.fishrig@gmail.com>
2016-11-30 10:17:07 +0100

    about and links

--- Test/config/routes.rb
+++ Test/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   get 'welcome/home', to: 'welcome#home'
+  get 'welcome/about', to: 'welcome#about'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
